ce1bacf7f689e198ab9931f119ee6f71cdf6586b
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2013-07-14 16:28:49 -0400

    Create getArticles.py

---
+++ getArticles.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+
+from BeautifulSoup import BeautifulSoup, NavigableString, BeautifulStoneSoup
+from urllib import urlopen
+from ununicode import toascii
+from datetime import datetime, timedelta
+from HTMLParser import HTMLParser
+import re
+import sys
+
+MINLENGTH = 800
+if len(sys.argv)>1 :
+    try:
+        MINLENGTH = int(sys.argv[1])
+    except:
+        pass
+    
+briefingUrl = "https://briefing.nextjump.com/?feed=rss2"
+briefConn = urlopen(briefingUrl)
+briefing = briefConn.read()
+
+soup = BeautifulStoneSoup(briefing, convertEntities=BeautifulSoup.HTML_ENTITIES)
+
+def strip_tags(html):
+    soup = BeautifulSoup(html, convertEntities=BeautifulSoup.HTML_ENTITIES)
+
+    for tag in soup.findAll(True):
+        s = ""
+
+        for c in tag.contents:
+            if not isinstance(c, NavigableString):
+                c = strip_tags(unicode(c))
+            s += unicode(c)
+
+        tag.replaceWith(s)
+
+    return soup
+
+def texify(str) :
+    simpleEscape = re.compile(r'([#$%&~_^{}|])')
+    mathEscape = re.compile(r'([<>])')
+    str = str.replace('\\', '\\textbacklash')
+    str = simpleEscape.sub(r'\\\1', str)
+    str = mathEscape.sub(r'$\1$', str)
+    return str
+    
+
+class Article :
+    def __init__(self, item) :
+        global MINLENGTH
+        self.title = toascii(unicode(item.title.string))
+        self.contributor = toascii(unicode(item.find('dc:creator').string))
+        self.href = toascii(unicode(item.link.string))
+        self.category = toascii(unicode(item.category.string))
+        self.content = ''
+        paragraphs = item.find('content:encoded')
+        paragraphs = BeautifulSoup(unicode(paragraphs.string), convertEntities=BeautifulSoup.HTML_ENTITIES).findAll('p')
+        i = 0
+        h = HTMLParser()
+        while len(self.content)<MINLENGTH and i<len(paragraphs) :
+            toAdd = h.unescape(toascii(unicode(strip_tags(unicode(paragraphs[i])))).strip()) 
+                #Gotta Unescape yer HTMLEntities before you can Escape them!
+            if len(toAdd) > 0 :
+                self.content = self.content + ' ' + toAdd
+            i+=1
+        #self.content = self.content.replace('\n', ' ')
+        self.content = re.compile(r'\r?\n').sub(' ', self.content)
+        #print i, len(self.content), len(paragraphs)
+
+
+        self.xml = item
+
+    def __str__(self) :
+        return self.href + ": (" \
+            +  self.category + ") " \
+            +  self.contributor + " posted \"" \
+            +  self.title + "\"\n\n" \
+            + self.content 
+    def csv(self) :
+        return texify(self.href) + "|" \
+            + texify(self.title) + "|" \
+            + texify(self.category) + "|" \
+            + texify(self.contributor) + "|" \
+            + texify(self.content)
+
+articles = []
+
+for item in  soup.findAll('item', limit=5) :
+    articles.append(Article(item))
+
+print "URL|Title|Category|Contributor|Text"
+
+for article in articles :
+    print article.csv()
